cfb2912fc52de87f6c8761bd8b55b414c3623634
Eduardo Contessi <contessi@outlook.com>
2022-04-25 19:46:01 -0300

    first commit

---
+++ src/main/java/io/github/educontessi/pixkeyregistration/PixkeyregistrationApplication.java
@@ -0,0 +1,13 @@
+package io.github.educontessi.pixkeyregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixkeyregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixkeyregistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/educontessi/pixkeyregistration/PixkeyregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.educontessi.pixkeyregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixkeyregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
